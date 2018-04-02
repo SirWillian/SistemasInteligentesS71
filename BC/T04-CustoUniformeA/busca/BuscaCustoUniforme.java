ec0cb698b238ae39c08b75c15f6c9085d4c829f4
terumiisobe <37278397+terumiisobe@users.noreply.github.com>
2018-04-02 19:46:13 -0300

    Add files via upload

--- BC/T04-CustoUniformeA/busca/BuscaCustoUniforme.java
+++ BC/T04-CustoUniformeA/busca/BuscaCustoUniforme.java
@@ -1,1 +1,70 @@
 package busca;
+
+import arvore.*;
+import sistema.Agente;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuscaCustoUniforme extends BuscaCega
+{
+    public BuscaCustoUniforme(Agente agnt)
+    {
+        super(agnt);
+    }
+
+    public int [] gerarSolucao()
+    {
+        List<TreeNode> explorados = new ArrayList<>();
+        TreeNode node;
+        int[] ap;
+
+        while(true)
+        {
+            if(this.fronteira.isEmpty())
+                return null; //Não achou nada
+            else
+            {
+                node=this.fronteira.remove(0); //"Pop" da fronteira
+                if(this.agente.prob.testeObjetivo(node.getState()))
+                    break;
+
+                explorados.add(node);
+
+                //Para cada ação possível, adiciona um nó na árvore de busca
+                ap=this.agente.prob.acoesPossiveis(node.getState());
+                for (int i=0; i<ap.length; i++)
+                {
+                    if(ap[i]!=-1)
+                    {
+                        TreeNode tmp = node.addChild();
+                        this.agente.treeSize++;
+                        tmp.setAction(i);
+                        tmp.setState(this.agente.prob.suc(node.getState(), i));
+                        tmp.setGn(node.getGn()+this.agente.prob.obterCustoAcao(node.getState(), i, tmp.getState()));
+
+                        boolean isExplorado=false;
+                        for(TreeNode each : explorados)
+                        {
+                            if(tmp.getState()==each.getState())
+                            {
+                                isExplorado=true;
+                                break;
+                            }
+                        }
+                        if(!isExplorado)
+                            addNaFronteira(tmp);
+                    }
+                }
+            }
+        }
+        int[] solucao = new int[node.getDepth()];
+        //Percorre de baixo pra cima na árvore
+        while(node!=this.arvore)
+        {
+            solucao[node.getDepth()-1]=node.getAction();
+            node=node.getParent();
+        }
+        //this.arvore.printSubTree();
+        return solucao;
+    }
+}
